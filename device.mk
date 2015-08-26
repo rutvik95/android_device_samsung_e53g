f3d4b4a7aabbabfa53923b5ecc912e9963d342d0
Jackeagle <jackeagle102@gmail.com>
2015-08-26 04:06:30 -0400

    configs: Add Media Configs
    Change-Id: I1ebc76dd957cb472c0a044dd03cad3173309b443
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Media configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
